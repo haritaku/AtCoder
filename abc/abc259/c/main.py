3463bc5bdc7a18e44b9ce0b7ad98339ddc7c8b45
haritaku <hari1147taku@gmail.com>
2022-07-10 01:44:40 +0900

    update: abc259, c

--- abc/abc259/c/main.py
+++ abc/abc259/c/main.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import groupby
 
 input = sys.stdin.readline
 
@@ -7,20 +8,10 @@ T = input().rstrip()
 
 
 def rle(string):
+    """Run Length Encoding"""
     rle_list = []
-    for i in range(len(string)):
-        if i == 0:
-            s, cnt = string[0], 1
-            continue
-
-        if string[i] == string[i - 1]:
-            cnt += 1
-        else:
-            rle_list.append((s, cnt))
-            s, cnt = string[i], 1
-
-        if i == len(string) - 1:
-            rle_list.append((s, cnt))
+    for k, group in groupby(string):
+        rle_list.append((k, len(tuple(group))))
     return rle_list
 
 
@@ -30,17 +21,13 @@ if len(S_rle) != len(T_rle):
     print("No")
     sys.exit()
 
-ans = "Yes"
 for s_rle, t_rle in zip(S_rle, T_rle):
-    if s_rle[0] != t_rle[0]:
-        ans = "No"
-        break
+    if s_rle[0] != t_rle[0] or s_rle[1] > t_rle[1]:
+        print("No")
+        sys.exit()
 
-    if s_rle[1] == t_rle[1]:
-        continue
-    elif s_rle[1] > 1 and s_rle[1] < t_rle[1]:
-        continue
-    else:
-        ans = "No"
-        break
-print(ans)
+    if s_rle[1] == 1 and s_rle[1] < t_rle[1]:
+        print("No")
+        sys.exit()
+
+print("Yes")
