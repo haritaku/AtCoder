b22f999760a4052efd254df51620c42c8682d6b0
haritaku <hari1147taku@gmail.com>
2022-07-18 00:57:49 +0900

    solve: abc260, d

--- abc/abc260/d/main.py
+++ abc/abc260/d/main.py
@@ -8,20 +8,20 @@ P = list(map(int, input().split()))
 
 ans = [-1] * N
 front = []
-deck = {}
+deck = []
 
 for i, p in enumerate(P, start=1):
     idx = bisect_left(front, p)
     if idx == len(front):
         front.append(p)
-        deck[p] = [p]
+        deck.append([p])
     else:
         front[idx] = p
-        deck[p] = deck.pop(front[idx]) + [p]
+        deck[idx].append(p)
 
-    if len(deck[p]) == K:
+    if len(deck[idx]) == K:
         del front[idx]
-        for j in deck.pop(p):
+        for j in deck.pop(idx):
             ans[j - 1] = i
 
 print(*ans, sep="\n")
