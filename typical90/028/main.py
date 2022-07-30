bc1ce3092cd7ff3472e59250b0f8ee218791c89a
haritaku <hari1147taku@gmail.com>
2022-07-30 23:19:35 +0900

    another solve: -28

--- typical90/028/main.py
+++ typical90/028/main.py
@@ -1,12 +1,14 @@
 import sys
 
+import numpy as np
+
 input = sys.stdin.readline
 
 N = int(input())
 rects = [list(map(int, input().split())) for _ in range(N)]
 M = 1_000
 
-field = [[0] * (M + 1) for _ in range(M + 1)]
+field = np.zeros((M + 1, M + 1), dtype=np.int64)
 
 for lx, ly, rx, ry in rects:
     field[lx][ly] += 1
@@ -14,14 +16,11 @@ for lx, ly, rx, ry in rects:
     field[lx][ry] -= 1
     field[rx][ly] -= 1
 
-for i in range(M):
-    for j in range(M):
-        field[i][j + 1] += field[i][j]
+field = np.cumsum(field, axis=0)
+field = np.cumsum(field, axis=1)
 
 ans = [0] * (N + 1)
-for j in range(M):
-    for i in range(M):
-        field[i + 1][j] += field[i][j]
-        ans[field[i][j]] += 1
-
+unis, cnts = np.unique(field, return_counts=True)
+for uni, cnt in zip(unis, cnts):
+    ans[uni] = cnt
 print(*ans[1:], sep="\n")
