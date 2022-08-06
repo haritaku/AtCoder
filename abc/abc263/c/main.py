4c29058f25a7656a14445d75048119d7839eef35
haritaku <hari1147taku@gmail.com>
2022-08-07 00:08:16 +0900

    another solve: abc263, c

--- abc/abc263/c/main.py
+++ abc/abc263/c/main.py
@@ -7,12 +7,4 @@ N, M = map(int, input().split())
 
 ans = []
 for arr in combinations(range(1, M + 1), r=N):
-    before = arr[0]
-    for val in arr[1:]:
-        if val < before:
-            break
-    else:
-        ans.append(arr)
-
-for arr in sorted(ans):
     print(*arr)
