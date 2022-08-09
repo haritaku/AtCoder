f0bc72034ae548504df98d5bf372ae274a47a8dd
haritaku <hari1147taku@gmail.com>
2022-08-10 00:10:25 +0900

    refactor: abc261, d

--- abc/abc261/d/main.py
+++ abc/abc261/d/main.py
@@ -5,15 +5,16 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 X = list(map(int, input().split()))
 CY = [list(map(int, input().split())) for _ in range(M)]
-CY = {c: y for c, y in CY}
+ref = [0] * (N + 1)
+for c, y in CY:
+    ref[c] = y
 
 dp = [0] * (N + 1)
-
 for i, x in enumerate(X, start=1):
     tmp = max(dp)
     for j in range(N, 0, -1):
         if j <= i:
-            dp[j] = dp[j - 1] + x + CY.get(j, 0)
+            dp[j] = dp[j - 1] + x + ref[j]
     dp[0] = tmp
 
 print(max(dp))
