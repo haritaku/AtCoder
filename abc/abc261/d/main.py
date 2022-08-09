3c37ac96406b8d999da846b475d4ffc95330464d
haritaku <hari1147taku@gmail.com>
2022-08-10 00:02:37 +0900

    another solve: abc261, d

--- abc/abc261/d/main.py
+++ abc/abc261/d/main.py
@@ -1,22 +1,19 @@
 import sys
 
 input = sys.stdin.readline
-INF = 10**18
 
 N, M = map(int, input().split())
 X = list(map(int, input().split()))
 CY = [list(map(int, input().split())) for _ in range(M)]
 CY = {c: y for c, y in CY}
 
-dp = [[-INF] * (N + 1) for _ in range(N + 1)]
+dp = [0] * (N + 1)
 
-max_ = 0
-dp[0][0] = 0
-for i in range(1, N + 1):
-    dp[i][0] = max_
-    max_ = 0
-    for j in range(1, N + 1):
-        dp[i][j] = dp[i - 1][j - 1] + CY.get(j, 0) + X[i - 1]
-        max_ = max(max_, dp[i][j])
+for i, x in enumerate(X, start=1):
+    tmp = max(dp)
+    for j in range(N, 0, -1):
+        if j <= i:
+            dp[j] = dp[j - 1] + x + CY.get(j, 0)
+    dp[0] = tmp
 
-print(max_)
+print(max(dp))
