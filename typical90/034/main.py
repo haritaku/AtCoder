af23c6a5eb1697ccea92ea349efd78c3fe09c40a
haritaku <hari1147taku@gmail.com>
2022-08-23 22:18:32 +0900

    refactor: 034

--- typical90/034/main.py
+++ typical90/034/main.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict, deque
+from collections import Counter, deque
 
 input = sys.stdin.readline
 
@@ -7,20 +7,17 @@ N, K = map(int, input().split())
 a_list = list(map(int, input().split()))
 
 q = deque()
-kind = 0
-d = defaultdict(int)
+c = Counter()
 ans = 0
 for a in a_list:
     q.append(a)
-    d[a] += 1
-    if d[a] == 1:
-        kind += 1
+    c[a] += 1
 
-    while q and kind > K:
+    while len(c) > K:
         rm = q.popleft()
-        d[rm] -= 1
-        if d[rm] == 0:
-            kind -= 1
+        c[rm] -= 1
+        if c[rm] == 0:
+            c.pop(rm)
     ans = max(ans, len(q))
 
 print(ans)
